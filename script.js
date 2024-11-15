d163b98a1f0b4b7adb7ed7a8c403d54d74fdf94d
eseferr <erginsefer@gmail.com>
2024-11-15 14:27:52 +0300

    Add input limit

--- script.js
+++ script.js
@@ -31,3 +31,25 @@ function operate(num1, num2, operation) {
     default:
   }
 }
+//Button Selections
+const numBtn = document.querySelectorAll(".btn-num");
+const screen = document.querySelector(".screen-text");
+const clearBtn = document.querySelector(".clear");
+let screenText = "";
+// Number Buttons Events
+numBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    if (screenText.length < 15) {
+      screenText += btn.textContent;
+      console.log(btn.textContent);
+      screen.textContent = screenText;
+      num1 = screenText;
+    }
+  });
+});
+// Clear Button Events
+clearBtn.addEventListener("click", () => {
+  num1 = 0;
+  textContent = "";
+  screen.textContent = 0;
+});
