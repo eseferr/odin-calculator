6289e61e622a1c308b1e8541745a795290e56260
eseferr <erginsefer@gmail.com>
2024-11-16 00:19:29 +0300

    Make calculator work

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let num1;
 let num2;
+let result;
 let operation;
 
 function add(num1, num2) {
@@ -8,48 +9,99 @@ function add(num1, num2) {
 function subtract(num1, num2) {
   return num1 - num2;
 }
-function multipy(num1, num2) {
+function multiply(num1, num2) {
   return num1 * num2;
 }
 function divide(num1, num2) {
-  return num1 / num2;
+  if (num2 !== 0) {
+    return num1 / num2;
+  } else {
+    return "Error";
+  }
 }
 function operate(num1, num2, operation) {
   switch (operation) {
     case "add":
-      add(num1, num2);
-      break;
+      return add(num1, num2);
     case "subtract":
-      subtract(num1, num2);
-      break;
+      return subtract(num1, num2);
     case "multiply":
-      multipy(num1, num2);
-      break;
+      return multiply(num1, num2);
     case "divide":
-      divide(num1, num2);
-      break;
+      return divide(num1, num2);
     default:
+      return 0;
   }
 }
 //Button Selections
 const numBtn = document.querySelectorAll(".btn-num");
 const screen = document.querySelector(".screen-text");
 const clearBtn = document.querySelector(".clear");
-let screenText = "";
+const operationBtns = document.querySelectorAll(".btn-operations");
+const equalBtn = document.querySelector(".equal");
+
 // Number Buttons Events
 numBtn.forEach((btn) => {
   btn.addEventListener("click", () => {
-    if (screenText.length < 15) {
-      screenText += btn.textContent;
-      console.log(btn.textContent);
-      screen.textContent = screenText;
-      num1 = screenText;
+    if (screen.textContent.length < 15 && screen.textContent == 0) {
+      screen.textContent = btn.textContent;
+    } else if (screen.textContent.length < 15) {
+      screen.textContent += btn.textContent;
     }
   });
 });
+
 // Clear Button Events
 clearBtn.addEventListener("click", () => {
   num1 = 0;
-  textContent = "";
-  screen.textContent = 0;
+  num2 = 0;
+  operation = "";
+  screen.textContent = "0";
+});
+
+//Operation Button Events
+operationBtns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const op = e.target.textContent;
+    if (screen.textContent !== "") {
+      switch (op) {
+        case "+":
+          num1 = parseFloat(screen.textContent);
+          num2 = 0;
+          operation = "add";
+          screen.textContent = "";
+          break;
+        case "-":
+          num1 = parseFloat(screen.textContent);
+          num2 = 0;
+          operation = "subtract";
+          screen.textContent = "";
+          break;
+        case "x":
+          num1 = parseFloat(screen.textContent);
+          num2 = 0;
+          operation = "multiply";
+          screen.textContent = "";
+          break;
+        case "/":
+          num1 = parseFloat(screen.textContent);
+          num2 = 0;
+          operation = "divide";
+          screen.textContent = "";
+          break;
+      }
+    }
+  });
+});
+equalBtn.addEventListener("click", () => {
+  if (screen.textContent !== "") {
+    console.log("sonunda!");
+    num2 = parseFloat(screen.textContent);
+    console.log(num1, num2, operation);
+    result = operate(num1, num2, operation);
+    screen.textContent = result.toString();
+    //num1 = result;
+    //num2 = null;
+    operation = "";
+  }
 });
